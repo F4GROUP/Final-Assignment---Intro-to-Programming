647ec08239e69c6dcf99e6704c65682d60022443
Tieu Binh <tieubinhco@gmail.com>
2018-05-28 22:17:07 +0700

    Gộp file

--- Final Assignment/Final Assignment_DOUBLE/A2Matrix_DOUBLE/equal_DOUBLE.c
+++ Final Assignment/Final Assignment_DOUBLE/A2Matrix_DOUBLE/equal_DOUBLE.c
@@ -9,3 +9,12 @@ int eql(double A, double B)
 
     //If the difference between A and B is less than the precision E, assume that A=B
 }
+
+int Neql(double A, double B)
+{
+    double E=0.0000001; // E is the precision
+
+    if(fabs(A-B)<=E) return 0;
+    else return 1;
+    //If the difference between A and B is less than the precision E, assume that A=B
+}
--- Final Assignment/Final Assignment_DOUBLE/A2Matrix_DOUBLE/matrixIO_DOUBLE.c
+++ Final Assignment/Final Assignment_DOUBLE/A2Matrix_DOUBLE/matrixIO_DOUBLE.c
@@ -1,4 +1,22 @@
+
 #include "A2Matrix_DOUBLE.h"
+void print_DOUBLE(double matrix[30][30],int R, int C)
+{
+    int i,j;
+    printf("\n\n");
+    for (i=1;i<=R;i++)
+
+    {
+        for (j=1;j<=C;j++)
+        printf("%10.10f\t\t",matrix[i][j]);
+
+        printf("\n\n");
+    }
+
+    printf("\n\n");
+}
+
+
 void enterIndex_DOUBLE(double matrix[30][30],int R, int C)
 
 {
@@ -22,4 +40,17 @@ void enterIndex_DOUBLE(double matrix[30][30],int R, int C)
         printf("\n");
     }
     printf("\n\n");
+}
+
+void enterRC_DOUBLE(int matrix[30][30],int *R, int *C)
+{
+    int r,c;
+    printf("Enter row of %s: ",matrix[0][0]);
+
+    printf("Enter column of %s: ",matrix[0][0]);
+    scanf("%d",&c);
+
+    *R=r;
+    *C=c;
+
 }
