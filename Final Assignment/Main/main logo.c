d9378bc80bfd49ca8b0dd11f4f14799a03cea816
Tieu Binh <tieubinhco@gmail.com>
2018-05-28 23:56:30 +0700

    ANew

--- Final Assignment/Main/main logo.c
+++ Final Assignment/Main/main logo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<windows.h>
-#include<conio.h>
+#include <windows.h>
+#include <conio.h>
 
 
 
@@ -19,8 +19,5 @@ void main_logo(int X1, int Y1)
     gotoxy(X1,Y1+8); printf("A project from Master C");
     gotoxy(X1,Y1+10);printf("Press enter to begin");
     getch();
-
     system("cls");
-
-
 }
--- Final Assignment/Main/selection1.c
+++ Final Assignment/Main/selection1.c
@@ -6,14 +6,11 @@ void selection(int X1,int Y1,char *select)
 {
     void gotoxy(int x,int y);
     gotoxy(X1,Y1+7);
-    gotoxy(X1,Y1+1); printf("Select type of matrices");
-    gotoxy(X1,Y1+2); printf("1. Integer number (int)");
-    gotoxy(X1,Y1+3); printf("2. Character (char)");
-    gotoxy(X1,Y1+4); printf("3. Real number (double)");
+    gotoxy(X1,Y1+1); printf("Select function");
+    gotoxy(X1,Y1+2); printf("1. Matrix Processing");
+    gotoxy(X1,Y1+3); printf("2. User Guide");
+    gotoxy(X1,Y1+4); printf("3. About this Project");
 
     gotoxy(X1,Y1+6); printf("0. Return to main page");
     gotoxy(X1,Y1+8); printf("_");scanf("%c",&select);
-
-
-
 }
